e0fbac5b882975c447cff613314e489ef58fba67
Sheila Hanson <slhanson11@live.com>
2024-04-06 14:57:58 -0500

    added apollo imports to app.jsx

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,7 +1,9 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+import { Outlet } from "react-router-dom";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
