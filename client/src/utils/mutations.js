941ffa5a6930e3bd687f91cb71b123c5bde5add0
Sheila Hanson <slhanson11@live.com>
2024-04-07 16:54:50 -0500

    updated coding error and updated a comment

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -33,7 +33,7 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookID!){
+  mutation removeBook($bookId: ID!) {
     removeBook(bookID: $bookID) {
       _id
       bookCount
--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
+      // Proxy /graphql requests to the GraphQL server
       '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
